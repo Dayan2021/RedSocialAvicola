5108620f5fb8e91e5647328fdde7144e2c152b51
Dayan2021 <83990169+Dayan2021@users.noreply.github.com>
2021-05-12 22:19:09 -0500

    Elaboracion de las diferentes categorias: avicultura,piscicultura,agricultura, porcicultura.

--- app/src/main/java/com/redsoc/redsocialavicola/activities/HomeActivity.java
+++ app/src/main/java/com/redsoc/redsocialavicola/activities/HomeActivity.java
@@ -1,16 +1,60 @@
 package com.redsoc.redsocialavicola.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.redsoc.redsocialavicola.R;
+import com.redsoc.redsocialavicola.fragments.ChatsFragment;
+import com.redsoc.redsocialavicola.fragments.FiltersFragment;
+import com.redsoc.redsocialavicola.fragments.HomeFragment;
+import com.redsoc.redsocialavicola.fragments.ProfileFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(new HomeFragment());
     }
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    if(item.getItemId() == R.id.itemHome){
+                        //FRAGMENT HOME
+                        openFragment(new HomeFragment());
+                    }
+                    else if(item.getItemId() == R.id.itemChats){
+                        //FRAGMENT CHATS
+                        openFragment(new ChatsFragment());
+                    }
+                    else if(item.getItemId() == R.id.itemFilters){
+                        //FRAGMENT FILTERS
+                        openFragment(new FiltersFragment());
+                    }
+                    else if(item.getItemId() == R.id.itemProfile){
+                        //FRAGMENT PROFILES
+                        openFragment(new ProfileFragment());
+                    }
+                    return true;
+                }
+            };
 }
---
+++ app/src/main/java/com/redsoc/redsocialavicola/fragments/HomeFragment.java
@@ -0,0 +1,53 @@
+package com.redsoc.redsocialavicola.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.redsoc.redsocialavicola.R;
+import com.redsoc.redsocialavicola.activities.PostActivity;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+    View mView;
+    FloatingActionButton mFab;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       mView= inflater.inflate(R.layout.fragment_home, container, false);
+       mFab = mView.findViewById(R.id.fab);
+       mFab.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+              goToPost();
+           }
+       });
+       return mView;
+    }
+
+    private void goToPost() {
+        Intent intent = new Intent(getContext(), PostActivity.class);
+        startActivity(intent);
+    }
+}
