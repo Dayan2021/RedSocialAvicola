0600cadbdb578150a79e6669c5009cb053bf8f91
Dayan2021 <83990169+Dayan2021@users.noreply.github.com>
2022-01-09 23:19:34 -0500

    FUNCIONALIDAD COMPLETA DE LIKE DE USUARIOS Y MOSTRAR LA CANTIDAD DE "ME GUSTAS" DISEÑO LIKES ME GUSTA Y NO ME GUSTA CREACION DE INDICE EN FIREBASE- MODIFICACION LIKES

--- app/src/main/java/com/redsoc/redsocialavicola/adapters/PostsAdapter.java
+++ app/src/main/java/com/redsoc/redsocialavicola/adapters/PostsAdapter.java
@@ -9,32 +9,46 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.redsoc.redsocialavicola.R;
 import com.redsoc.redsocialavicola.activities.PostDetailActivity;
+import com.redsoc.redsocialavicola.models.Like;
 import com.redsoc.redsocialavicola.models.Post;
+import com.redsoc.redsocialavicola.providers.AuthProvider;
+import com.redsoc.redsocialavicola.providers.LikesProvider;
 import com.redsoc.redsocialavicola.providers.PostProvider;
 import com.redsoc.redsocialavicola.providers.UsersProvider;
 import com.squareup.picasso.Picasso;
+
+import java.util.Date;
 
 public class PostsAdapter extends FirestoreRecyclerAdapter<Post,PostsAdapter.ViewHolder> {
 
     Context context;
     UsersProvider mUsersProvider;
+    LikesProvider mLikesProvider;
+    AuthProvider  mAuthProvider;
 
     public PostsAdapter(FirestoreRecyclerOptions<Post> options, Context context) {
         super(options);
         this.context = context;
         mUsersProvider = new UsersProvider();
+        mLikesProvider = new LikesProvider();
+        mAuthProvider = new AuthProvider();
+
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull Post post) {
+    protected void onBindViewHolder(@NonNull final ViewHolder holder, int position, @NonNull final Post post) {
 
         DocumentSnapshot document = getSnapshots().getSnapshot(position);
         final String postId = document.getId();
@@ -55,8 +69,74 @@ public class PostsAdapter extends FirestoreRecyclerAdapter<Post,PostsAdapter.Vie
             }
         });
 
+        holder.imageViewLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Like like = new Like();
+                like.setIdUser(mAuthProvider.getUid());
+                like.setIdPost(postId);
+                like.setTimestamp(new Date().getTime());
+                like(like,holder);
+            }
+        });
+
+
         getUserInfo(post.getIdUser(), holder);
+        getNumberLikesByPost(postId, holder);
+        checkIfExistLike(postId, mAuthProvider.getUid(), holder);
     }
+
+    private void getNumberLikesByPost(String idPost, final ViewHolder holder) {
+        mLikesProvider.getLikesByPost(idPost).addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                int numberLikes = queryDocumentSnapshots.size();
+                holder.textViewLikes.setText(String.valueOf(numberLikes) + " Me gustas");
+            }
+        });
+    }
+
+
+
+    private void like(final Like like, final ViewHolder holder) {
+        mLikesProvider.getLikeByPostAndUser(like.getIdPost(), mAuthProvider.getUid()).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                int numberDocuments = queryDocumentSnapshots.size();
+                if (numberDocuments > 0) {
+                    String idLike = queryDocumentSnapshots.getDocuments().get(0).getId();
+                    holder.imageViewLike.setImageResource(R.drawable.like_gray);
+                    mLikesProvider.delete(idLike);
+                }
+                else {
+                    holder.imageViewLike.setImageResource(R.drawable.like_blue);
+                    mLikesProvider.create(like);
+                }
+            }
+        });
+
+    }
+
+
+    private void checkIfExistLike(String idPost, String idUser, final ViewHolder holder) {
+        mLikesProvider.getLikeByPostAndUser(idPost, idUser).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                int numberDocuments = queryDocumentSnapshots.size();
+                if (numberDocuments > 0) {
+                    holder.imageViewLike.setImageResource(R.drawable.like_blue);
+                }
+                else {
+                    holder.imageViewLike.setImageResource(R.drawable.like_gray);
+                }
+            }
+        });
+
+    }
+
+
+
+
 
     private void getUserInfo(String idUser, final ViewHolder holder) {
         mUsersProvider.getUser(idUser).addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -85,7 +165,9 @@ public class PostsAdapter extends FirestoreRecyclerAdapter<Post,PostsAdapter.Vie
         TextView textViewTitle;
         TextView textViewDescription;
         TextView textViewUsername;
+        TextView textViewLikes;
         ImageView imageViewPost;
+        ImageView imageViewLike;
         View viewHolder;
 
         public ViewHolder(View view) {
@@ -93,7 +175,9 @@ public class PostsAdapter extends FirestoreRecyclerAdapter<Post,PostsAdapter.Vie
             textViewTitle = view.findViewById(R.id.textViewTitlePostCard);
             textViewDescription = view.findViewById(R.id.textViewDescriptionPostCard);
             textViewUsername = view.findViewById(R.id.textViewUsernamePostCard);
+            textViewLikes = view.findViewById(R.id.textViewLikes);
             imageViewPost = view.findViewById(R.id.imageViewPostCard);
+            imageViewLike = view.findViewById(R.id.imageViewLike);
             viewHolder = view;
         }
     }
